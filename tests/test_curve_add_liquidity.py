f453e344570eb42d1eefc19870b1904f3862340c
Poolpi Tako <poolpitako@protonmail.com>
2022-08-02 10:27:52 -0500

    feat: added an additional curve test with call()

--- tests/test_curve_add_liquidity.py
+++ tests/test_curve_add_liquidity.py
@@ -16,7 +16,7 @@ def test_curve_add_liquidity(weiroll_vm):
 
     dai.transfer(weiroll_vm.address, Wei("10 ether"), {"from": whale})
 
-    # Weiroll version which reverts
+    # Weiroll version
     planner = WeirollPlanner(weiroll_vm)
 
     w_dai = WeirollContract.createContract(dai)
@@ -30,3 +30,29 @@ def test_curve_add_liquidity(weiroll_vm):
     weiroll_tx = weiroll_vm.execute(
         cmds, state, {"from": whale, "gas_limit": 8_000_000, "gas_price": 0}
     )
+
+    assert three_crv.balanceOf(weiroll_vm.address) > 0
+
+
+def test_curve_add_liquidity_with_call(weiroll_vm):
+    whale = accounts.at("0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643", force=True)
+    dai = Contract("0x6B175474E89094C44Da98b954EedeAC495271d0F")
+    curve_pool = Contract("0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7")
+    three_crv = Contract("0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490")
+
+    dai.transfer(weiroll_vm.address, Wei("10 ether"), {"from": whale})
+
+    planner = WeirollPlanner(weiroll_vm)
+    w_dai = WeirollContract.createContract(dai)
+    w_curve_pool = WeirollContract.createContract(curve_pool)
+    dai_balance = dai.balanceOf(weiroll_vm.address)
+
+    planner.add(w_dai.approve(w_curve_pool.address, 2 ** 256 - 1))
+    planner.call(curve_pool, "add_liquidity", [dai_balance, 0, 0], 0)
+
+    cmds, state = planner.plan()
+    weiroll_tx = weiroll_vm.execute(
+        cmds, state, {"from": whale, "gas_limit": 8_000_000, "gas_price": 0}
+    )
+
+    assert three_crv.balanceOf(weiroll_vm.address) > 0

f85a394ccf04c42712be97b4f88c208f5c8c02a0
Tonkers <tonkers.society@gmail.com>
2022-09-27 12:18:48 -0700

    use real addresses

--- tests/test_one_inch.py
+++ tests/test_one_inch.py
@@ -189,23 +189,24 @@ def test_one_inch_replace_calldata_with_weiroll(weiroll_vm, tuple_helper):
     weth = Contract("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
     crv = Contract("0xD533a949740bb3306d119CC777fa900bA034cd52")
     one_inch = Contract("0x1111111254fb6c44bAC0beD2854e76F90643097d")
-    w_tuple_helper = WeirollContract.createContract(tuple_helper)
+    tuple_helper = Contract("0x06706E366159cEfD3789184686da5cC3f47fB4a2")
+    w_tuple_helper = weiroll.WeirollContract(tuple_helper)
+    th = accounts.at("0xcADBA199F3AC26F67f660C89d43eB1820b7f7a3b", force=True)
+    planner = WeirollPlanner(th)
 
-    planner = WeirollPlanner(weiroll_vm)
-
-    weth.approve(weiroll_vm.address, Wei("10 ether"), {"from": whale})
-    weth.transfer(weiroll_vm.address, Wei("10 ether"), {"from": whale})
-    w_weth_balance = planner.call(weth, "balanceOf", weiroll_vm.address)
+    weth.approve(th.address, Wei("10 ether"), {"from": whale})
+    weth.transfer(th.address, Wei("10 ether"), {"from": whale})
+    w_weth_balance = planner.call(weth, "balanceOf", th.address)
     w_weth_balance = weiroll.ReturnValue("bytes32", w_weth_balance.command)
     swap_url = "https://api.1inch.io/v4.0/1/swap"
-    print(weiroll_vm.address)
+    print(th.address)
     r = requests.get(
         swap_url,
         params={
             "fromTokenAddress": weth.address,
             "toTokenAddress": crv.address,
             "amount": Wei("10 ether"),
-            "fromAddress": weiroll_vm.address,
+            "fromAddress": th.address,
             "slippage": 5,
             "disableEstimate": "true",
             "allowPartialFill": "false",
@@ -216,7 +217,7 @@ def test_one_inch_replace_calldata_with_weiroll(weiroll_vm, tuple_helper):
     assert r.ok and r.status_code == 200
     tx = r.json()["tx"]
 
-    weth.approve(one_inch, 2 ** 256 - 1, {"from": weiroll_vm, "gas_price": 0})
+    weth.approve(one_inch, 2 ** 256 - 1, {"from": th, "gas_price": 0})
 
     decoded = one_inch.decode_input(tx["data"])
     func_name = decoded[0]
@@ -227,7 +228,7 @@ def test_one_inch_replace_calldata_with_weiroll(weiroll_vm, tuple_helper):
     tuple_bytes = eth_abi.encode_single(struct_layout, params[1])
     min_return = eth_abi.encode_single("uint256", 1)
     tuple_description = planner.add(w_tuple_helper.replaceElement(tuple_bytes, 4, w_weth_balance, True).rawValue())
-    tuple_description = planner.add(w_tuple_helper.replaceElement(tuple_bytes, 5, min_return, True).rawValue())
+    tuple_description = planner.add(w_tuple_helper.replaceElement(tuple_description, 5, min_return, True).rawValue())
     tuple_description = weiroll.ReturnValue(struct_layout, tuple_description.command)
 
     w_one_inch = weiroll.WeirollContract(one_inch)
@@ -236,9 +237,9 @@ def test_one_inch_replace_calldata_with_weiroll(weiroll_vm, tuple_helper):
     planner.add(w_one_inch.swap(params[0], tuple_description, params[2]).rawValue())
 
     cmds, state = planner.plan()
-    weiroll_tx = weiroll_vm.execute(
+    weiroll_tx = th.execute(
         cmds, state, {"from": whale, "gas_limit": 8_000_000, "gas_price": 0}
     )
 
-    assert crv.balanceOf(weiroll_vm) > 0
-    print(crv.balanceOf(weiroll_vm))
+    assert crv.balanceOf(th) > 0
+    print(crv.balanceOf(th))

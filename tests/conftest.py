73ef43d8d62300099c87e0e9aa7ce8bf4a390818
FP <fp_crypt@protonmail.com>
2022-10-05 17:09:48 -0700

    fix: tests remove unneeded block

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,5 @@
 import pytest
-from brownie import config, Contract, network
-import requests
 from weiroll import WeirollContract
-
-# @pytest.fixture(scope="session", autouse=True)
-# def _tenderly_fork():
-#    import requests
-#    import brownie
-#
-#    fork_base_url = "https://simulate.yearn.network/fork"
-#    payload = {"network_id": "1"}
-#    resp = requests.post(fork_base_url, headers={}, json=payload)
-#    fork_id = resp.json()["simulation_fork"]["id"]
-#    fork_rpc_url = f"https://rpc.tenderly.co/fork/{fork_id}"
-#    print(fork_rpc_url)
-#    tenderly_provider = brownie.web3.HTTPProvider(fork_rpc_url, {"timeout": 600})
-#    brownie.web3.provider = tenderly_provider
-#    print(f"https://dashboard.tenderly.co/yearn/yearn-web/fork/{fork_id}")
 
 
 @pytest.fixture(scope="function", autouse=True)
